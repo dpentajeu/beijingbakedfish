d1745fae5a10d5e54ff6f6f4caf067559b472a86
Darren <darren.chee@pentajeu.com>
2013-07-07 16:17:49 +0800

    add analytics

--- protected/views/layouts/main2.php
+++ protected/views/layouts/main2.php
@@ -27,6 +27,21 @@ $packages = array('1'=>'Silver RM500', '2'=>'Gold RM1500', '3'=>'Titanium RM3500
 	<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bnb.js" type="text/javascript"></script>
 	<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/modernizr.js" type="text/javascript"></script>
 
+    <!--  Analytics  -->
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-42274384-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
+
 <script type="text/javascript">
 
   var _gaq = _gaq || [];
