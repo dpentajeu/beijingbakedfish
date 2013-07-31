d55ebf9643ba30f10f50508ec40c68fe8d1e776e
LCC <leechechian@gmail.com>
2013-07-31 15:37:50 +0800

    update network view

--- protected/views/member/network.php
+++ protected/views/member/network.php
@@ -2,7 +2,7 @@
         <div class="h_title">Network</div>
         <h2>Network Hierarchy</h2>
         <p>Show the sponsor network level</p>
-        <div class="n_warning"><p>Red color indicated member is not activated.</p></div>
+        <div class="n_warning"><p>Red color indicated member is not activate.</p></div>
         <div>
                 <div class="h_title" style="text-align: center;">Sponsor Bonus Guidelines</div>
                 <div class="stats">
