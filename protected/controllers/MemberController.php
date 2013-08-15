2383aec37ae67b5946ce469b19cdca8b0452dee3
Darren <darren.chee@pentajeu.com>
2013-08-15 12:59:15 +0800

    add manual and revert sponsor bonus function

--- protected/controllers/MemberController.php
+++ protected/controllers/MemberController.php
@@ -19,7 +19,7 @@ class MemberController extends Controller
 	{
 		return array(
 			array('allow',
-				'actions'=>array('login','logout','resetpassword', 'test5'),
+				'actions'=>array('login','logout','resetpassword', 'manualsponsorbonus', 'revertsponsorbonus'),
 				'users'=>array('*'),
 				),
 			array('allow',
@@ -638,11 +638,30 @@ class MemberController extends Controller
             $this->render('refermember', array('model'=>$model, 'packages'=>$packages, 'CMessage'=>$CMessage, 'notice'=>$notice));
     }
 
-    public function actionTest5()
+    public function actionManualsponsorbonus($id = null)
     {
-    	network::setSponsorBonus(40);
-    	network::setSponsorBonus(41);
-    	network::setSponsorBonus(42);
-    	network::setSponsorBonus(43);
+    	network::setSponsorBonus($id);
+    	var_dump("Successfully grant Sponsor bonus of {$id}");
+    }
+
+    public function actionRevertsponsorbonus($id = null)
+    {
+    	$user = User::model()->findByAttributes(array('id'=>$id));
+    	$criteria = new CDbCriteria;
+		$criteria->addSearchCondition('description', "Sponsor bonus from {$user->name}");
+		$transaction = Transaction::model()->findAll($criteria);
+			
+		foreach($transaction as $t)
+		{
+			$wallet = Wallet::model()->findByAttributes(array('id'=>$t->walletId));
+			$wallet->cashPoint -= $t->amount;
+			if(!$wallet->save())
+				var_dump("Transaction id: {$t->id} failed to deduct from wallet.");
+
+			if(!$t->delete())
+				var_dump("Transaction id: {$t->id} failed deleted and amount is deducted from {$wallet->user->name} wallet (CP: {$wallet->cashPoint}).");
+			else
+				var_dump("Transaction id: {$t->id} deleted and amount is deducted from {$wallet->user->name} wallet (CP: {$wallet->cashPoint}).<br/>");
+		}    	
     }
 }
